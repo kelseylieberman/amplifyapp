890188e3810be594fe79491e7aa3e08d59ca74c4
Kelsey Lieberman <kelsey.lieberman@kcftech.com>
2022-05-24 15:32:22 -0600

    added auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Amplify } from 'aws-amplify';
+
+import { Authenticator, Button } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import './App.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Desk Lite!</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+      <main className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome to Desk Lite with authentication, {user.username}!</h1>
+          <Button variation='primary' size="large" onClick={signOut} className="colorful-button">Sign out</Button>
+        </header>
+      </main>
+    )}
+    </Authenticator>
   );
 }
 
